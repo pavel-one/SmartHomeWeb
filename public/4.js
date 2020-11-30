0e35b219798b8f335c4558f9dbfa88534baff5b9
Pavel Zarubin <pavel@orendat.ru>
2020-11-30 14:02:11 +0300

    Remove trash js

--- public/4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{18:function(t,e,n){"use strict";n.r(e);var i={props:["title"],metaInfo:function(){return{title:this.title}}},l=n(0),r=Object(l.a)(i,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);e.default=r.exports}}]);
